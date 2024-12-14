11f4f41c42bbbffb38b3329902de4e0b8befab3c
AS2200118 <146171755+AS2200118@users.noreply.github.com>
2024-12-14 13:52:20 +0200

    web compiled with error
    thr website compiled with error required to instakl web vitals

--- myrestaurantapp/src/Components/NavBar.js
+++ myrestaurantapp/src/Components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "../Images/RestaurantLogo.jpg";
+import { Link } from "react-router-dom";
 
 function NavBar() {
     return (
@@ -7,7 +8,13 @@ function NavBar() {
             <div className="leftside">
                 <img src={Logo} />
             </div>
-            <div className="rightside"></div>
+            <div className="rightside">
+                <Link to="/"> Home</Link>
+                <Link to="/menu"> Menu</Link>
+                <Link to="/reservation"> Reservation</Link>
+                <Link to="/login"> Login</Link>
+                <Link to="/register"> Register</Link>
+            </div>
         </div>
     );
 }
