7d27c59c21a61c277fcd0207d11c0277d67e7f59
AS2200118 <146171755+AS2200118@users.noreply.github.com>
2024-12-14 14:42:55 +0200

    Update NavBar.js
    Updated NavBar to have the button functioning, true to open and false to close

--- myrestaurantapp/src/Components/NavBar.js
+++ myrestaurantapp/src/Components/NavBar.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../Images/RestaurantLogo.jpg";
 import { Link } from "react-router-dom";
 import ReorderIcon from '@mui/icons-material/Reorder';
 import "../CSSstyle/NavBar.css"
 
 function NavBar() {
+    const [openLinks, setOpenLinks] = useState(false);
+    const toggleNavBar = ()=> {
+        setOpenLinks(!openLinks)
+    };
     return (
         <div className="navbar">
-            <div className="leftside">
+            <div className="leftside" id={openLinks ? "open" : "close"}>
                 <img src={Logo} />
+                <div className="hiddenLinks">
+                    <Link to="/"> Home</Link>
+                    <Link to="/menu"> Menu</Link>
+                    <Link to="/reservation"> Reservation</Link>
+                    <Link to="/login"> Login</Link>
+                    <Link to="/register"> Register</Link>
+                </div>
             </div>
             <div className="rightside">
                 <Link to="/"> Home</Link>
@@ -16,7 +27,7 @@ function NavBar() {
                 <Link to="/reservation"> Reservation</Link>
                 <Link to="/login"> Login</Link>
                 <Link to="/register"> Register</Link>
-                <button>
+                <button onClick={toggleNavBar}>
                     <ReorderIcon />
                 </button>
             </div>
