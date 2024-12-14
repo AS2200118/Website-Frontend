95d409dc318a7dc3e29b84ea6154aa27e63fc45f
AS2200118 <146171755+AS2200118@users.noreply.github.com>
2024-12-14 15:17:43 +0200

    Update Footer.js
    Added a copyright logo and the date of this website to show that this is copyrighted.
    Added socials so the users can contact us

--- myrestaurantapp/src/Components/Footer.js
+++ myrestaurantapp/src/Components/Footer.js
@@ -1,8 +1,17 @@
 import React from "react";
+import InstagramIcon from '@mui/icons-material/Instagram';
+import XIcon from '@mui/icons-material/X';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
 
 function Footer() {
     return (
-        <div>
+        <div className="footer">
+            <div className="socialMedia">
+                <InstagramIcon /> <FacebookIcon /> <XIcon /> <WhatsAppIcon />
+            </div>
+            <p> &copy; 2024 Reserve-Now.com</p>
         </div>
     );
 }
