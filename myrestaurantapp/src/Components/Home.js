51368a9f13ca04a6fa2a78f1e2897fde3d479896
AS2200118 <146171755+AS2200118@users.noreply.github.com>
2024-12-14 14:58:35 +0200

    Added banner imagae
    Added banner image for the home page

--- myrestaurantapp/src/Components/Home.js
+++ myrestaurantapp/src/Components/Home.js
@@ -1,11 +1,12 @@
 import { Button } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
+import BannerImage from "../Images/BannerImage.webp"
 
 function Home() {
     return (
         <div className="home">
-            <div className="headerContainer">
+            <div className="headerContainer" style={{ backgroundImage: `url(${BannerImage})` }}>
                 <h1> Reserve-Now </h1>
                 <p> RESERVE YOUR FAVOURITE RESTAURANT </p>
                 <Link to="/menu">
