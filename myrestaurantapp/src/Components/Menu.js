60f4f7ad21945d5cd3f5e338842ddbe43c4592f9
AS2200118 <146171755+AS2200118@users.noreply.github.com>
2024-12-14 15:25:57 +0200

    Update Menu.js
    Addd basic handler to the menu.js. Added a title to show this is the menu

---
+++ myrestaurantapp/src/Components/Menu.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Menu() {
+    return (
+        <div className="menu">
+            <h1 className="menuTitle">The Menu</h1>
+            <div></div>
+        </div>
+    )
+}
