7b02abd5542f77af1c8a6e3d603cd744d9275848
AS2200118 <146171755+AS2200118@users.noreply.github.com>
2024-12-14 14:27:33 +0200

    Added MU icon
    I added the material- ui icon for the list

--- myrestaurantapp/src/Components/NavBar.js
+++ myrestaurantapp/src/Components/NavBar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Logo from "../Images/RestaurantLogo.jpg";
 import { Link } from "react-router-dom";
+import ReorderIcon from '@mui/icons-material/Reorder';
+import "../CSSstyle/NavBar.css"
 
 function NavBar() {
     return (
@@ -14,6 +16,9 @@ function NavBar() {
                 <Link to="/reservation"> Reservation</Link>
                 <Link to="/login"> Login</Link>
                 <Link to="/register"> Register</Link>
+                <button>
+                    <ReorderIcon />
+                </button>
             </div>
         </div>
     );
