ca0b3295570bdff42729095a6f0f9aad2109ff59
AS2200118 <146171755+AS2200118@users.noreply.github.com>
2024-12-14 14:52:30 +0200

    started on Home.js
    This will be the main page

--- myrestaurantapp/src/Components/Home.js
+++ myrestaurantapp/src/Components/Home.js
@@ -1,7 +1,19 @@
+import { Button } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
-    return <div> Welcome to my restaurant</div>
+    return (
+        <div className="home">
+            <div className="headerContainer">
+                <h1> Reserve-Now </h1>
+                <p> RESERVE YOUR FAVOURITE RESTAURANT </p>
+                <Link to="/menu">
+                    <Button> CHECK OUR MENU </Button>
+                </Link>
+            </div>
+        </div>
+    );
 }
 
 export default Home
